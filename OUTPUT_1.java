0517da83b746cbe2663b9469bbacfdff1d61c599
shruthi-sai <89636224+shruthi-sai@users.noreply.github.com>
2021-10-03 19:38:48 +0530

    OUTPUT_1.java

---
+++ OUTPUT_1.java
@@ -0,0 +1,19 @@
+public class OUTPUT_1
+{
+	class Main
+	{
+		static int a = m1();
+		static {
+			System.out.println("Inside static block");
+		}
+		static int m1() {
+			System.out.println("from m1");
+			return 20;
+			}
+		public static void main(String[] args)
+		{
+			System.out.println("Value of a : "+a);
+			System.out.println("from main");
+			}
+		}
+}
